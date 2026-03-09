67e69bea41e4f0536693e38cc2328282fa2e917b
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-03-09 16:08:21 +0530

    feat: Add ContainerScroll component for enhanced scroll animation and integrate it into the Open Source Tracker page

--- frontend/src/app/opensource/page.tsx
+++ frontend/src/app/opensource/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
 
 function page() {
   return (
-    <div>
-      
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <h1 className="text-4xl font-semibold text-white">
+            DevTrack <br />
+            <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+              Open Source Tracker
+            </span>
+          </h1>
+        }
+      >
+        <img
+          src="/opensource.png"
+          alt="opensource"
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
